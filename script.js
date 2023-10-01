1a0efed4a3fa6a08664921cc742ae347ba7600b0
davi <davisousaferreira187@gmail.com>
2023-10-01 20:10:39 -0300

    arquivos adicionados

---
+++ script.js
@@ -0,0 +1,105 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const cartItemsList = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    const checkoutButton = document.getElementById('checkout-button');
+    const paymentOption = document.getElementById('payment-method');
+    const deliveryOption = document.getElementById('select_delivery');
+    const customerAddress = document.getElementById('input_address');
+    const Address = document.getElementById('address');
+    const clientNameInput = document.getElementById('client_name');
+
+    let cart = [];
+
+    // Função para atualizar o carrinho
+    function updateCart() {
+        cartItemsList.innerHTML = '';
+        let total = 0;
+        const itemDetails = [];
+
+        cart.forEach((item, index) => {
+            const li = document.createElement('li');
+            li.innerHTML = `${item.name} - R$ ${item.price.toFixed(2)}`;
+            
+            // Adiciona um botão para excluir o item
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Excluir';
+            deleteButton.classList.add('cart-button', 'delete');
+            deleteButton.addEventListener('click', () => removeFromCart(index));
+
+            li.appendChild(deleteButton);
+
+            cartItemsList.appendChild(li);
+            total += item.price;
+            itemDetails.push(`${item.name} - R$ ${item.price.toFixed(2)}`);
+        });
+
+        cartTotal.textContent = `R$ ${total.toFixed(2)}`;
+        return itemDetails;
+    }
+
+    // Adicionar item ao carrinho
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const name = this.getAttribute('data-name');
+            const price = parseFloat(this.getAttribute('data-price'));
+            cart.push({ name, price });
+            updateCart();
+        });
+    });
+
+    // Remover item do carrinho
+    function removeFromCart(index) {
+        cart.splice(index, 1);
+        updateCart();
+    }
+
+    // Evento ouvinte para o select do Tipo de entrega
+    deliveryOption.addEventListener("change", function () {
+        if (deliveryOption.value === "Entregar no endereço") {
+            // Mostra o input de texto do endereço de entrega
+            Address.style.display = "block";
+        } else {
+            // Esconde o input de texto do endereço de entrega
+            Address.style.display = "none";
+        }
+    });
+
+    // Finalizar compra
+    checkoutButton.addEventListener('click', function () {
+        if (cart.length > 0) {
+            const payment = paymentOption.value;
+            const delivery = deliveryOption.value;
+            const address = customerAddress.value;
+            const total = parseFloat(cartTotal.textContent.replace('R$ ', ''));
+            const clientName = clientNameInput.value;
+
+            // Obtenha a lista de detalhes dos itens do carrinho
+            const itemDetails = updateCart();
+
+            // Construa a mensagem para o WhatsApp
+            let mensagemWhatsApp = `Pedido de ${clientName}:\n`;
+
+            // Adicione os detalhes dos itens do carrinho à mensagem
+            itemDetails.forEach((item) => {
+                mensagemWhatsApp += `${item}\n`;
+            });
+
+            mensagemWhatsApp += `\nTotal: R$ ${total.toFixed(2)}\nTipo de Pagamento: ${payment}\nTipo de Entrega: ${delivery}\nEndereço de Entrega: ${address}`;
+
+            // Número de telefone para o qual você deseja enviar a mensagem (no formato internacional)
+            const numeroTelefone = "5585998006527";
+
+            // Construir a URL do WhatsApp com o número de telefone e a mensagem
+            const urlWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroTelefone + "&text=" + encodeURIComponent(mensagemWhatsApp);
+
+            // Abrir uma nova aba no navegador para o WhatsApp
+            window.open(urlWhatsApp, "_blank");
+
+            cart = [];
+            updateCart();
+        } else {
+            alert('Carrinho vazio. Adicione itens antes de finalizar o pedido.');
+        }
+    });
+});
